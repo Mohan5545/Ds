418d87de2fc82f86163e7a81f6a8f18aff03b37d
Mohan <48092723+Mohan5545@users.noreply.github.com>
2019-07-17 10:34:05 +0530

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,26 @@
+class Stack:
+    def __init__(self,size=0):
+        self.size = size
+        self.list1=[]
+        
+    def add(self,data):
+        if len(self.list1)>=self.size:
+            print("Stack is full")
+            return
+        self.list1.append(data)
+    
+    def remove(self):
+        if len(self.list1)<=0:
+            print("Stack is empty")
+            return
+        data=self.list1.pop()
+        return data
+    
+    def printStack(self):
+        for ele in self.list1:
+            print(ele)
+
+obj = Stack(5)
+obj.add(00)
+obj.add(10)
+obj.printStack()
